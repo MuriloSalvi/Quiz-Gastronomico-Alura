a687eefe9f4342496a2d7cf782ad7cc9e62aa31a
Murilo Salvi <murilo_salvi@outloo.com>
2021-01-27 16:42:15 -0300

    Second Commit

---
+++ src/components/StartButton/index.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components'
+
+const StartButton = styled.button`
+background-color: ${({ theme }) => theme.colors.item};
+margin-top: 3
+px;
+width: 100%;
+color: ${({theme})=> theme.colors.contrastText};
+padding: 8px;
+border-radius: 10px;
+outline: none;
+border: 2px solid ${({theme})=>theme.colors.secondary}
+`
+
+export default StartButton;
