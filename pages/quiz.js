25ea69a5aff85874e63f233c7ac64ae771a3be30
MuriloSalvi <murilo_salvi@outlook.com>
2021-01-28 10:43:21 -0300

    Update quiz.js

--- pages/quiz.js
+++ pages/quiz.js
@@ -1,4 +1,16 @@
 import React from 'react';
+import styled from 'styled-components';
+import Head from 'next/head';
+import { useRouter } from 'next/router';
+
+import db from '../db.json';
+import Widget from '../src/components/Widget';
+import QuizLogo from '../src/components/QuizLogo';
+import QuizBackground from '../src/components/QuizBackground';
+import Footer from '../src/components/Footer';
+import GitHubCorner from '../src/components/GitHubCorner';
+import Label from '../src/components/Label';
+import StartButton from '../src/components/StartButton';
 
 export default function QuizPage() {
   return (
