0bebb5951a80b9bad667c76e4530cf241ab7956e
MuriloSalvi <murilo_salvi@outlook.com>
2021-01-28 10:43:27 -0300

    Update index.js

--- src/components/StartButton/index.js
+++ src/components/StartButton/index.js
@@ -1,3 +1,4 @@
+import PropTypes from 'prop-types';
 import styled from 'styled-components';
 
 const StartButton = styled.button`
@@ -9,6 +10,22 @@ padding: 8px;
 border-radius: 10px;
 outline: none;
 border: 2px solid ${({ theme }) => theme.colors.secondary};
+cursor: pointer;
+box-shadow: 0px 5px 5px rgb(0,0,0, 0.5);
+position: relative;
+
+&:hover{
+    opacity: 0.5
+}
+&:active{
+    top: 3px;
+    box-shadow: none;
+}
 `;
 
+StartButton.propTypes = {
+    type: PropTypes.oneOf(['submit', 'type', 'button']).isRequired,
+    children: PropTypes.node.isRequired,
+};
+// eslint-disable-next-line eol-last
 export default StartButton;
