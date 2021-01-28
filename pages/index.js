70adbcf6ade9fb598e7b619571f0ff7ae9757339
MuriloSalvi <murilo_salvi@outlook.com>
2021-01-28 10:43:18 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -4,35 +4,26 @@ import Head from 'next/head';
 import { useRouter } from 'next/router';
 
 import db from '../db.json';
+import QuizContainer from '../src/components/QuizContainer';
 import Widget from '../src/components/Widget';
 import QuizLogo from '../src/components/QuizLogo';
 import QuizBackground from '../src/components/QuizBackground';
 import Footer from '../src/components/Footer';
 import GitHubCorner from '../src/components/GitHubCorner';
-import Label from '../src/components/Label'
-import StartButton from '../src/components/StartButton'
+import Label from '../src/components/Label';
+import StartButton from '../src/components/StartButton';
 
 // const BackgroundImage = styled.div`
-//   background-image: url(${db.bg});
 //   flex: 1;
 //   background-size: cover;
 //   background-position: center;
 // `;
 
-export const QuizContainer = styled.div`
-  width: 100%;
-  max-width: 350px;
-  padding-top: 45px;
-  margin: auto;
-  @media screen and (max-width: 500px) {
-    margin: auto;
-    padding: 15px;
-  }
-`;
+
 
 export default function Home() {
   const router = useRouter();
-  const [name, setName] = React.useState('')
+  const [name, setName] = React.useState('');
 
   return (
     <QuizBackground backgroundImage={db.bg}>
@@ -46,20 +37,26 @@ export default function Home() {
             <h1>Quiz Gastronomico</h1>
           </Widget.Header>
           <Widget.Content>
-            <form onSubmit={function(event){
+            <form onSubmit={function (event) {
               event.preventDefault();
-            
-              router.push(`/quiz?name=${name}`);
-              console.log('Funcionou')
 
-              //router manda direto pra proxima pagina
-            }}>
-            <Label placeholder='Seu nome' onChange={function(event){
-              setName(event.target.value)
-            }}></Label>
-            <StartButton type='submit' type='submit' disabled={name.length == 0}>
-              jogar {name}
-            </StartButton>
+              router.push(`/quiz?name=${name}`);
+              console.log('Funcionou');
+
+              // router manda direto pra proxima pagina
+            }}
+            >
+              <Label
+              name = 'nomeDoUsuario'
+                onChange={ (event) =>{
+                  setName(event.target.value);
+                }}
+                placeholder="Seu nome"
+                value={name}
+                 />
+              <StartButton type="submit" type="submit" disabled={name.length == 0}>
+                jogar {name}
+              </StartButton>
             </form>
           </Widget.Content>
         </Widget>
