33787db65b9de280ddaea26d70aaba351352739c
MuriloSalvi <murilo_salvi@outlook.com>
2021-01-28 09:24:50 -0300

    Update index.js

--- src/components/StartButton/index.js
+++ src/components/StartButton/index.js
@@ -1,16 +1,14 @@
-
-import styled from 'styled-components'
+import styled from 'styled-components';
 
 const StartButton = styled.button`
 background-color: ${({ theme }) => theme.colors.item};
 margin-top: 13px;
 width: 100%;
-color: ${({theme})=> theme.colors.contrastText};
+color: ${({ theme }) => theme.colors.contrastText};
 padding: 8px;
 border-radius: 10px;
 outline: none;
-border: 2px solid ${({theme})=>theme.colors.secondary}
-`
-
+border: 2px solid ${({ theme }) => theme.colors.secondary};
+`;
 
 export default StartButton;
