32489de04b77d3749c623aea9d563bacec62efa8
MuriloSalvi <murilo_salvi@outlook.com>
2021-01-28 10:43:23 -0300

    Update index.js

--- src/components/Label/index.js
+++ src/components/Label/index.js
@@ -1,6 +1,8 @@
+import React from 'react';
+import PropTypes from 'prop-types';
 import styled from 'styled-components';
 
-const Label = styled.input`
+const InputBase = styled.input`
 background-color: ${({ theme }) => theme.colors.contrastText};
 border: 1px solid ${({ theme }) => theme.colors.mainBg};
 width: 100%;
@@ -12,4 +14,24 @@ text-align: center;
 outline: none;
 `;
 
-export default Label;
+export default function Label({ onChange, placeholder, ...props }) {
+  return (
+    <div>
+      <InputBase
+        placeholder={placeholder}
+        onChange={onChange}
+        {...props}
+      />
+    </div>
+  );
+}
+Label.deafaultProps = {
+  value: '',
+};
+
+Label.propTypes = {
+  onChange: PropTypes.func.isRequired,
+  placeholdeer: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  value: PropTypes.string.isRequired,
+};
